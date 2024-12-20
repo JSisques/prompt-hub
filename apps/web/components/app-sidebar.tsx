d0afdb3f538c5a0058331236a4d1ba869b7b7dc0
JSisques <javierplazasisques@gmail.com>
2024-12-20 22:10:13 +0100

    📝 Automatic changes
    ✏️ Title: Automated code update
    
    📅 Date: 2024-12-20 22:10:13
    
    📝 Modified files: - apps/web/app/auth/layout.tsx
    - apps/web/app/auth/login/page.tsx
    - apps/web/app/layout.tsx
    - apps/web/app/page.tsx
    - apps/web/components.json
    - apps/web/components/app-sidebar.tsx
    - apps/web/components/login-form.tsx
    - apps/web/components/ui/avatar.tsx
    - apps/web/components/ui/badge.tsx
    - apps/web/components/ui/button.tsx
    - apps/web/components/ui/card.tsx
    - apps/web/components/ui/checkbox.tsx
    - apps/web/components/ui/dropdown-menu.tsx
    - apps/web/components/ui/input.tsx
    - apps/web/components/ui/label.tsx
    - apps/web/components/ui/nav-user.tsx
    - apps/web/components/ui/navigation-menu.tsx
    - apps/web/components/ui/pagination.tsx
    - apps/web/components/ui/progress.tsx
    - apps/web/components/ui/select.tsx
    - apps/web/components/ui/separator.tsx
    - apps/web/components/ui/sheet.tsx
    - apps/web/components/ui/sidebar.tsx
    - apps/web/components/ui/skeleton.tsx
    - apps/web/components/ui/switch.tsx
    - apps/web/components/ui/textarea.tsx
    - apps/web/components/ui/toast.tsx
    - apps/web/components/ui/toaster.tsx
    - apps/web/components/ui/tooltip.tsx
    - apps/web/hooks/use-mobile.tsx
    - apps/web/hooks/use-toast.ts
    - apps/web/lib/utils.ts
    - apps/web/package.json
    - apps/web/postcss.config.js
    - apps/web/styles/globals.css
    - apps/web/tailwind.config.js
    - apps/web/tsconfig.json
    - package-lock.json
    - packages/ui-interface/components/ui/navigation-menu.tsx
    - packages/ui-interface/package.json
    - yarn.lock

---
+++ apps/web/components/app-sidebar.tsx
@@ -0,0 +1,241 @@
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Calendar,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Home,
+  Inbox,
+  PieChart,
+  Search,
+  Settings,
+  Settings2,
+  SquareTerminal,
+  Terminal,
+} from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from '@/components/ui/sidebar';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { NavUser } from './ui/nav-user';
+import Link from 'next/link';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  teams: [
+    {
+      name: 'Acme Inc',
+      logo: GalleryVerticalEnd,
+      plan: 'Enterprise',
+    },
+    {
+      name: 'Acme Corp.',
+      logo: AudioWaveform,
+      plan: 'Startup',
+    },
+    {
+      name: 'Evil Corp.',
+      logo: Command,
+      plan: 'Free',
+    },
+  ],
+  navMain: [
+    {
+      title: 'Playground',
+      url: '#',
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: 'History',
+          url: '#',
+        },
+        {
+          title: 'Starred',
+          url: '#',
+        },
+        {
+          title: 'Settings',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Models',
+      url: '#',
+      icon: Bot,
+      items: [
+        {
+          title: 'Genesis',
+          url: '#',
+        },
+        {
+          title: 'Explorer',
+          url: '#',
+        },
+        {
+          title: 'Quantum',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Documentation',
+      url: '#',
+      icon: BookOpen,
+      items: [
+        {
+          title: 'Introduction',
+          url: '#',
+        },
+        {
+          title: 'Get Started',
+          url: '#',
+        },
+        {
+          title: 'Tutorials',
+          url: '#',
+        },
+        {
+          title: 'Changelog',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings2,
+      items: [
+        {
+          title: 'General',
+          url: '#',
+        },
+        {
+          title: 'Team',
+          url: '#',
+        },
+        {
+          title: 'Billing',
+          url: '#',
+        },
+        {
+          title: 'Limits',
+          url: '#',
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: 'Design Engineering',
+      url: '#',
+      icon: Frame,
+    },
+    {
+      name: 'Sales & Marketing',
+      url: '#',
+      icon: PieChart,
+    },
+    {
+      name: 'Travel',
+      url: '#',
+      icon: Map,
+    },
+  ],
+};
+
+// Menu items.
+const items = [
+  {
+    title: 'Prompts',
+    url: '/prompts',
+    icon: Terminal,
+  },
+  {
+    title: 'Categories',
+    url: '/categories',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Prompt Hub</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
